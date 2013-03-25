bb2d292c8a4a853237dfbb51ef666a1c67d994b4
Hannes Mehnert <hannes@mehnert.org>
2013-03-25 11:48:07 +0100

    clarify sources and license

--- inferior-idris-mode.el
+++ inferior-idris-mode.el
@@ -5,11 +5,25 @@
 ;; Author: David Raymond Christiansen <drc@itu.dk>, Hannes Mehnert <hame@itu.dk>
 ;; Keywords: languages
 
+;; License:
+;; Inspiration and some methods have been taken from haskell-mode (https://github.com/haskell/haskell-mode/)
+;; Other inspiration is taken from SLIME/DIME (http://common-lisp.net/project/slime/) (https://github.com/dylan-lang/dylan-mode)
+;; Therefore license is GPL
 
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
 
-;;; Commentary:
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;; GNU General Public License for more details.
 
-;; 
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING. If not, write to
+;; the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
 
 ;;; Code:
 (require 'comint)
