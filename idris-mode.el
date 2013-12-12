72847ba6a278d067fae565fad13d3550e75793bd
Hannes Mehnert <hannes@mehnert.org>
2013-12-12 11:42:08 +0100

    fix typo

--- idris-mode.el
+++ idris-mode.el
@@ -9,7 +9,7 @@
 
 ;;; Commentary:
 
-;; This is an Emacs mode for editing Idris code. It requires the lastest
+;; This is an Emacs mode for editing Idris code. It requires the latest
 ;; version of Idris, and some features may rely on the latest Git version of
 ;; Idris.
 
