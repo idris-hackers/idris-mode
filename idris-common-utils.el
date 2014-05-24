fe6a78dc685c2ef1ff0b431ef5cd6dad9fd2fb05
David Raymond Christiansen <david@davidchristiansen.dk>
2014-05-24 21:02:45 +0200

    Improve clickable filename code
    This now uses overlays instead of text properties, making it much less
    likely to conflict with minor modes or commands that set certain
    properties.

--- idris-common-utils.el
+++ idris-common-utils.el
@@ -173,6 +173,16 @@ corresponding values in the CDR of VALUE."
 BUFFER is not supplied or is nil."
   (string= (file-name-extension (buffer-file-name buffer)) "lidr"))
 
+(defun idris-make-file-link-overlay (start end keymap help-echo)
+  (let ((overlay (make-overlay start end)))
+    (overlay-put overlay 'name 'idris-file-link)
+    (overlay-put overlay 'keymap keymap)
+    (overlay-put overlay 'mouse-face 'highlight)
+    (overlay-put overlay 'help-echo help-echo)))
+
+(defun idris-clear-file-link-overlays ()
+  "Remove all file link overlays from the current buffer"
+  (remove-overlays nil nil 'idris-file-link))
 
 (defun idris-make-module-link (start end src-dir)
   "Attempt to make the region between START and END into a
@@ -189,10 +199,7 @@ relative to SRC-DIR"
                    (define-key map [mouse-2] #'(lambda ()
                                                  (interactive)
                                                  (find-file src-name)))
-                   (put-text-property start end 'keymap map)
-                   (put-text-property start end 'mouse-face 'highlight)
-                   (put-text-property start end 'help-echo
-                                      "mouse-2: edit module"))))
+                   (idris-make-file-link-overlay start end map "mouse-2: edit module"))))
       (if (file-exists-p idr)
           (make-link idr)
         (when (file-exists-p lidr)
