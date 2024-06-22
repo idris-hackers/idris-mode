ca7ac397112d2e2fbe80e6ab40ec6d440a3c5c2e
Marek L <nospam.keram@gmail.com>
2024-06-22 15:30:49 +0100

    Simplify retrieving value of sourcedir property in
    *.ipkg file by extracting the lookup to a function `idris-ipkg-buffer-sourcedir`
    
    Additionaly match also underscore and dash in ipkg sourcedir regexp

--- idris-ipkg-mode.el
+++ idris-ipkg-mode.el
@@ -87,10 +87,6 @@
 (defconst idris-ipkg-font-lock-defaults
   `(,idris-ipkg-keywords))
 
-(defconst idris-ipkg-sourcedir-re
-  "^sourcedir\\s-*=\\s-*\"?\\([a-zA-Z/0-9]+\\)\"?")
-;; "^\\s-*sourcedir\\s-*=\\s-*\\(\\sw+\\)"
-
 ;;; Completion
 
 (defun idris-ipkg-find-keyword ()
@@ -142,19 +138,17 @@
   (idris-clear-file-link-overlays 'idris-ipkg-mode)
   (let ((src-dir (idris-ipkg-buffer-src-dir (file-name-directory (buffer-file-name)))))
     ;; Make the sourcedir clickable
-    (save-excursion
-      (goto-char (point-min))
-      (when (and (file-exists-p src-dir)
-                 (file-directory-p src-dir)
-                 (re-search-forward idris-ipkg-sourcedir-re nil t))
-        (let ((start (match-beginning 1))
-              (end (match-end 1))
-              (map (make-sparse-keymap)))
-          (define-key map [mouse-2] #'(lambda ()
-                                        (interactive)
-                                        (dired src-dir)))
-          (idris-make-file-link-overlay start end map
-                                        (concat "mouse-2: dired " src-dir)))))
+    (when (and (file-exists-p src-dir)
+               (file-directory-p src-dir)
+               (idris-ipkg-buffer-sourcedir-point))
+      (let ((start (match-beginning 1))
+            (end (match-end 1))
+            (map (make-sparse-keymap)))
+        (define-key map [mouse-2] #'(lambda ()
+                                      (interactive)
+                                      (dired src-dir)))
+        (idris-make-file-link-overlay start end map
+                                      (concat "mouse-2: dired " src-dir))))
     ;; Make the modules clickable
     (save-excursion
       (goto-char (point-min))
@@ -304,13 +298,17 @@ arguments."
   (interactive)
   (idris-kill-buffer idris-ipkg-build-buffer-name))
 
-(defun idris-ipkg-buffer-src-dir (basename)
+(defun idris-ipkg-buffer-sourcedir-point ()
+  "Return nil or a point at the end of sourcedir value in the current ipkg file."
   (save-excursion
     (goto-char (point-min))
-    (if-let ((found (re-search-forward idris-ipkg-sourcedir-re nil t)))
-        (concat (file-name-directory basename)
-                (buffer-substring-no-properties (match-beginning 1) (match-end 1)))
-      (file-name-directory basename))))
+    (re-search-forward "^sourcedir\\s-*=\\s-*\"?\\([A-Za-z0-9_-]+\\)\"?" nil t)))
+
+(defun idris-ipkg-buffer-src-dir (basename)
+  (if (idris-ipkg-buffer-sourcedir-point)
+      (concat (file-name-directory basename)
+              (buffer-substring-no-properties (match-beginning 1) (match-end 1)))
+    (file-name-directory basename)))
 
 (defun idris-ipkg-find-src-dir (&optional ipkg-file)
   (let ((found (or (and ipkg-file (list ipkg-file))
